7604ee53cae327e209f56650d00fa36393f1c2f2
chuklin <inkarttatoo@gmail.com>
2023-06-26 19:59:14 +0500

    added title_func and docstring to str_func

--- str_func.py
+++ str_func.py
@@ -1,3 +1,8 @@
 def str_func(word):
+    """Return the word where every letter is upper"""
     return word.upper()
 
+
+def title_func(word):
+    """Return the word with first title letter"""
+    return word.title()
